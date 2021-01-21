9df45116b17d2bc3e8ab65e57b78b2aa277c033f
anirrudh <anik597@gmail.com>
2021-01-21 17:10:56 -0500

    update submodules

--- automerge/rust/src/lib.rs
+++ automerge/rust/src/lib.rs
@@ -10,6 +10,7 @@ use simplelog::*;
 
 mod ambackend;
 mod load;
+mod nbformatbackend;
 
 // The main python module - jupyter_rtc_automerge
 #[pymodule]
@@ -26,9 +27,12 @@ fn jupyter_rtc_automerge(py: Python, module: &PyModule) -> PyResult<()> {
 
     let submod = PyModule::new(py, "automerge")?;
     let submod_load = PyModule::new(py, "load")?;
+    let submod_nbformatbackend = PyModule::new(py, "nbf")?;
     ambackend::init_submodule(submod)?;
     load::init_submodule(submod_load)?;
+    nbformatbackend::init_submodule(submod_nbformatbackend)?;
     module.add_submodule(submod)?;
     module.add_submodule(submod_load)?;
+    module.add_submodule(submod_nbformatbackend)?;
     Ok(())
 }
