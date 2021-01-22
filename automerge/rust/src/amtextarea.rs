15459e0a5198eaf556c487fc8fdf32b804fd585a
Pierre-Olivier Simonard <pierre.olivier.simonard@gmail.com>
2021-01-22 17:13:32 +0100

    textarea automerge backend : update + test

--- automerge/rust/src/amtextarea.rs
+++ automerge/rust/src/amtextarea.rs
@@ -1,16 +1,10 @@
 // Python Wrappers
-use pyo3::conversion::FromPyObject;
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
 use pyo3::wrap_pyfunction;
 // Automerge Libraries
 use automerge_backend;
 use automerge_frontend;
 use automerge_protocol;
-use log::{debug, info, LevelFilter};
-use simplelog::*;
-use std::any::Any;
-use std::collections::HashMap;
 
 fn default_document(doc_id: &str, default_text: &str) -> automerge_backend::Backend {
     let mut doc_backend = automerge_backend::Backend::init();
@@ -70,7 +64,7 @@ fn new_document(doc_id: &str, default_text: &str) -> std::vec::Vec<u8> {
 
 #[pyfunction]
 fn apply_change(doc: std::vec::Vec<u8>, change_data: std::vec::Vec<u8>) -> std::vec::Vec<u8> {
-    let mut backend = automerge_backend::Backend::load(doc)
+    let mut doc_backend = automerge_backend::Backend::load(doc)
         .and_then(|back| Ok(back))
         .unwrap();
 
@@ -78,29 +72,23 @@ fn apply_change(doc: std::vec::Vec<u8>, change_data: std::vec::Vec<u8>) -> std::
         .and_then(|c| Ok(c))
         .unwrap();
 
-    backend
+    doc_backend
         .apply_changes(vec![change])
         .and_then(|patch| Ok(patch))
         .unwrap();
 
-    let doc = backend.save().and_then(|data| Ok(data));
+    let doc_data = doc_backend.save().and_then(|data| Ok(data));
 
-    return doc.unwrap();
-}
-
-#[pyfunction]
-fn consume_notebook(nb: &PyDict) {
-    info!("the notebook model has been sent, {}", nb);
-    // let v: HashMap = nb.extract()?;
+    return doc_data.unwrap();
 }
 
 #[pyfunction]
 fn get_changes(doc: std::vec::Vec<u8>) -> std::vec::Vec<std::vec::Vec<u8>> {
-    let backend = automerge_backend::Backend::load(doc)
+    let doc_backend = automerge_backend::Backend::load(doc)
         .and_then(|back| Ok(back))
         .unwrap();
 
-    let changes = backend.get_changes(&[]);
+    let changes = doc_backend.get_changes(&[]);
 
     let mut changes_bytes: std::vec::Vec<std::vec::Vec<u8>> = std::vec::Vec::new();
 
@@ -115,6 +103,25 @@ pub fn init_submodule(module: &PyModule) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(new_document, module)?)?;
     module.add_function(wrap_pyfunction!(apply_change, module)?)?;
     module.add_function(wrap_pyfunction!(get_changes, module)?)?;
-    module.add_function(wrap_pyfunction!(consume_notebook, module)?)?;
     Ok(())
 }
+
+#[test]
+fn test_new_document() {
+
+    // Simple test over the critical path : create document, get changes.
+    
+    // apply_changes will have to be tested :
+    // - here, instanciating an automerge frontend and generating a patch of changes, and checking the document changed
+    // - through an automated UI test using a text area
+    
+    let doc_data = new_document("test_doc_id", "Test content");
+    let changes_data = get_changes(doc_data);
+    
+
+    // There must be two changes : one to set the doc id, one to set the content
+    assert_eq!( changes_data.len(), 2  );
+    
+    
+}
+
