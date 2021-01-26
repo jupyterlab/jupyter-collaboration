9039842d40d1a87c730d5ddb7dbe24579e361268
anirrudh <anik597@gmail.com>
2021-01-26 00:36:32 -0500

    cleanup tests

--- automerge/rust/tests/test_backend.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import jupyter_rtc_automerge
-
-def test_notebook_model():
-    jupyter_rtc_automerge.automerge.consume_notebook(jupyter_rtc_automerge.init_notebook())
-    return
-
--- automerge/rust/tests/test_nbformatbackend.py
+++ automerge/rust/tests/test_nbformatbackend.py
@@ -4,5 +4,5 @@ import nbformat
 
 def test_backend():
     test_nb = nbformat.v4.new_notebook()
-    f = jupyter_rtc_automerge.nbf.initialize_nbdoc(pynb=test_nb)
+    f = jupyter_rtc_automerge.nb.serialize_notebook(test_nb)
     return
