a84a4bb1f091805809c3e345fb392ceb8d10bc3e
David Brochart <david.brochart@gmail.com>
2022-11-14 13:18:29 +0100

    Move configuration from JupyterLab (#54)
    * Move configuration from JupyterLab
    
    * Review

--- jupyter_server_ydoc/__init__.py
+++ jupyter_server_ydoc/__init__.py
@@ -1,30 +1,9 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
-import jupyter_server.serverapp
-from jupyter_server.utils import url_path_join
-
-from .ydoc import YDocRoomIdHandler, YDocWebSocketHandler
+from .app import YDocExtension
 
 __version__ = "0.2.0"
 
 
-def _jupyter_server_extension_points() -> List[Dict[str, str]]:
-    return [{"module": "jupyter_server_ydoc"}]
-
-
-def _load_jupyter_server_extension(serverapp: jupyter_server.serverapp.ServerApp) -> None:
-    web_app = serverapp.web_app
-    host_pattern = ".*$"
-    yid_route_pattern = url_path_join(web_app.settings["base_url"], r"/api/yjs/roomid/(.*)")
-    yws_route_pattern = url_path_join(web_app.settings["base_url"], r"/api/yjs/(.*)")
-    web_app.add_handlers(
-        host_pattern,
-        [
-            (yid_route_pattern, YDocRoomIdHandler),
-            (yws_route_pattern, YDocWebSocketHandler),
-        ],
-    )
-
-
-# Backward compatibility for classic notebook based start-up (e.g. Binder)
-load_jupyter_server_extension = _load_jupyter_server_extension
+def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
+    return [{"module": "jupyter_server_ydoc", "app": YDocExtension}]
---
+++ jupyter_server_ydoc/app.py
@@ -0,0 +1,68 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+try:
+    from jupyter_server.extension.application import ExtensionApp
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Jupyter Server must be installed to use this extension.")
+
+from traitlets import Float, Int, Type
+from ypy_websocket.ystore import BaseYStore  # type: ignore
+
+from .handlers import JupyterSQLiteYStore, YDocRoomIdHandler, YDocWebSocketHandler
+
+
+class YDocExtension(ExtensionApp):
+
+    name = "jupyter_server_ydoc"
+
+    file_poll_interval = Int(
+        1,
+        config=True,
+        help="""The period in seconds to check for file changes on disk.
+        Defaults to 1s, if 0 then file changes will only be checked when
+        saving changes from the front-end.""",
+    )
+
+    document_cleanup_delay = Int(
+        60,
+        allow_none=True,
+        config=True,
+        help="""The delay in seconds to keep a document in memory in the back-end after all clients
+        disconnect. Defaults to 60s, if None then the document will be kept in memory forever.""",
+    )
+
+    document_save_delay = Float(
+        1,
+        allow_none=True,
+        config=True,
+        help="""The delay in seconds to wait after a change is made to a document before saving it.
+        Defaults to 1s, if None then the document will never be saved.""",
+    )
+
+    ystore_class = Type(
+        default_value=JupyterSQLiteYStore,
+        klass=BaseYStore,
+        config=True,
+        help="""The YStore class to use for storing Y updates. Defaults to JupyterSQLiteYStore,
+        which stores Y updates in a '.jupyter_ystore.db' SQLite database in the current
+        directory.""",
+    )
+
+    def initialize_settings(self):
+        self.settings.update(
+            {
+                "collaborative_file_poll_interval": self.file_poll_interval,
+                "collaborative_document_cleanup_delay": self.document_cleanup_delay,
+                "collaborative_document_save_delay": self.document_save_delay,
+                "collaborative_ystore_class": self.ystore_class,
+            }
+        )
+
+    def initialize_handlers(self):
+        self.handlers.extend(
+            [
+                (r"/api/yjs/roomid/(.*)", YDocRoomIdHandler),
+                (r"/api/yjs/(.*)", YDocWebSocketHandler),
+            ]
+        )
