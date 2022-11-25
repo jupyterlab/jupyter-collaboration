616e989fa73e1d6371ca08993dda94ba1f619f2b
David Brochart <david.brochart@gmail.com>
2022-11-25 09:31:49 +0100

    Remove metadata

--- jupyter_server_ydoc/handlers.py
+++ jupyter_server_ydoc/handlers.py
@@ -3,7 +3,6 @@
 
 import asyncio
 import json
-from datetime import datetime
 from logging import Logger
 from pathlib import Path
 from typing import Any, Dict, Optional, Tuple
@@ -55,13 +54,6 @@ class TransientRoom(YRoom):
         super().__init__(log=log)
 
 
-async def metadata_callback() -> bytes:
-    # the current datetime will be stored in metadata as bytes
-    # it can be retrieved as:
-    # datetime.fromisoformat(metadata.decode())
-    return datetime.utcnow().isoformat().encode()
-
-
 class JupyterWebsocketServer(WebsocketServer):
 
     rooms: Dict[str, YRoom]
@@ -91,9 +83,7 @@ class JupyterWebsocketServer(WebsocketServer):
                 file_format, file_type, file_path = path.split(":", 2)
                 p = Path(file_path)
                 updates_file_path = str(p.parent / f".{file_type}:{p.name}.y")
-                ystore = self.ystore_class(
-                    path=updates_file_path, metadata_callback=metadata_callback, log=self.log
-                )
+                ystore = self.ystore_class(path=updates_file_path, log=self.log)
                 self.rooms[path] = DocumentRoom(file_type, ystore, self.log)
             else:
                 # it is a transient document (e.g. awareness)
