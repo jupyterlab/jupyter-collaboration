2b852e202b1d0b0f404178b39746b2d87ec73ed8
Carlos Herrero <contact@carloshb.com>
2023-01-30 18:11:07 +0100

    Create a JupyterLab extension

--- jupyter_server_ydoc/__init__.py
+++ jupyter_server_ydoc/__init__.py
@@ -4,6 +4,12 @@ from .app import YDocExtension
 
 __version__ = "0.7.0"
 
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": "@jupyterlab/rtc-extension"
+    }]
+
 
 def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
     return [{"module": "jupyter_server_ydoc", "app": YDocExtension}]
