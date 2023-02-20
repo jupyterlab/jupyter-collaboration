9d187ac32c0f617c958f5223f7b0edd5c97be9f1
Frédéric Collonval <fcollonval@gmail.com>
2023-02-20 11:52:52 +0100

    Add binder

---
+++ binder/jupyter_config.py
@@ -0,0 +1,12 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import logging
+
+c.ServerApp.log_level = logging.DEBUG
+
+c.LabApp.collaborative = True
+c.LabApp.extensions_in_dev_mode = True
+
+c.ContentsManager.allow_hidden = True
+# Use advance file ID service for out of band rename support
+c.FileIdExtension.file_id_manager_class = "jupyter_server_fileid.manager.LocalFileIdManager"
