f32d89617c134bd39861465f37e92a14f40cddaf
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-03-03 14:06:27 +0100

    Apply suggestions from code review

--- jupyter_server_ydoc/__init__.py
+++ jupyter_server_ydoc/__init__.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List
 
 from .app import YDocExtension
 
-__version__ = "0.6.2"
+__version__ = "0.8.0"
 
 
 def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
