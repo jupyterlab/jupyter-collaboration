b2a32be5a293dae1afca6fe2fa32609d43b3b8aa
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-03-05 11:21:21 +0000

    Publish 0.8.0
    SHA256 hashes:
    
    jupyter_server_ydoc-0.8.0-py3-none-any.whl: 969a3a1a77ed4e99487d60a74048dc9fa7d3b0dcd32e60885d835bbf7ba7be11
    
    jupyter_server_ydoc-0.8.0.tar.gz: a6fe125091792d16c962cc3720c950c2b87fcc8c3ecf0c54c84e9a20b814526c

--- jupyter_server_ydoc/__init__.py
+++ jupyter_server_ydoc/__init__.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List
 
 from .app import YDocExtension
 
-__version__ = "0.8.0a0"
+__version__ = "0.8.0"
 
 
 def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
