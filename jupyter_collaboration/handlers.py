78c340cefdbab8b26e36cb7c5f5816f60b1e796e
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2023-04-05 15:38:57 +0200

    More docstring

--- jupyter_collaboration/handlers.py
+++ jupyter_collaboration/handlers.py
@@ -43,28 +43,61 @@ class JupyterWebsocketServer(WebsocketServer):
         self.ypatch_nb = 0
         self.connected_users = {}
         self.background_tasks = set()
-        self.monitor_task = asyncio.create_task(self.monitor())
+        self.monitor_task = asyncio.create_task(self._monitor())
 
-    async def monitor(self):
+    def room_exists(self, path: str) -> bool:
+        """
+        Returns true is the room exist or false otherwise.
+
+            Parameters:
+                path (str): Room ID.
+
+            Returns:
+                exists (bool): Whether the room exists or not.
+        """
+        return path in self.rooms
+
+    def add_room(self, path: str, room: YRoom) -> None:
+        """
+        Adds a new room.
+
+            Parameters:
+                path (str): Room ID.
+                room (YRoom): A room.
+        """
+        self.rooms[path] = room
+
+    def get_room(self, path: str) -> YRoom:
+        """
+        Returns the room for the specified room ID or raises a RoomNotFound
+        error if the room doesn't exist.
+
+            Parameters:
+                path (str): Room ID.
+
+            Returns:
+                room (YRoom): The room.
+        """
+        if path not in self.rooms:
+            # Document rooms need a file
+            raise RoomNotFound
+
+        return self.rooms[path]
+
+    async def _monitor(self):
+        """
+        An infinite loop with a 60 seconds delay for counting the number
+        of patches processed in a minute and how many clients are connected.
+
+        #### Note:
+            This method runs in a coroutine for debugging purposes.
+        """
         while True:
             await asyncio.sleep(60)
             clients_nb = sum(len(room.clients) for room in self.rooms.values())
             self.log.info("Processed %s Y patches in one minute", self.ypatch_nb)
             self.log.info("Connected Y users: %s", clients_nb)
             self.ypatch_nb = 0
-
-    def room_exists(self, path: str) -> bool:
-        return path in self.rooms
-
-    def add_room(self, path: str, room: YRoom) -> None:
-        self.rooms[path] = room
-
-    def get_room(self, path: str) -> YRoom:
-        if path not in self.rooms:
-            # Document rooms need a file
-            raise RoomNotFound
-
-        return self.rooms[path]
 
 
 class YDocWebSocketHandler(WebSocketHandler, JupyterHandler):
