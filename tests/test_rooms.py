1cbbe8ba19abd88fdf1282bf511066c276c328d3
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2023-07-28 14:58:48 +0200

    Update tests/test_rooms.py
    Co-authored-by: Frédéric Collonval <fcollonval@users.noreply.github.com>

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -86,6 +86,7 @@ async def test_should_not_save_content_after_document_change():
     await room.initialize()
     room._document.source = "Test 2"
 
+    # Wait for a bit more than the poll_interval
     await asyncio.sleep(0.15)
 
     assert "save" not in cm.actions
