54310b08cf4d366dcdf6cb7e7936b0b2aac04d33
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2023-07-31 10:32:44 +0200

    lint

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -96,7 +96,7 @@ async def test_undefined_save_delay_should_not_save_content_after_document_chang
 async def test_should_reload_content_from_disk():
     id = "test-id"
     content = "test"
-    paths = { id: "test.txt" }
+    paths = {id: "test.txt"}
     last_modified = datetime.now()
     cm = FakeContentsManager({"last_modified": last_modified, "content": "whatever"})
     loader = FileLoader(
