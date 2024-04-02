33f7a1fd072011ad12d1707e6263bcfe1fde95b0
krassowski <5832902+krassowski@users.noreply.github.com>
2024-04-02 10:24:33 +0100

    Try to delete jupyter_server_fileid early

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -40,6 +40,14 @@ async def test_dirty(
                 assert not jupyter_ydoc.dirty
 
 
+async def cleanup(jp_serverapp):
+    # workaround for a shutdown issue of aiosqlite, see
+    # https://github.com/jupyterlab/jupyter-collaboration/issues/252
+    await jp_serverapp.web_app.settings["jupyter_collaboration"].stop_extension()
+    # workaround `jupyter_server_fileid` manager accessing database on GC
+    del jp_serverapp.web_app.settings["file_id_manager"]
+
+
 async def test_room_concurrent_initialization(
     jp_serverapp,
     rtc_create_file,
@@ -61,9 +69,7 @@ async def test_room_concurrent_initialization(
     t1 = time()
     assert t1 - t0 < 0.5
 
-    # workaround for a shutdown issue of aiosqlite, see
-    # https://github.com/jupyterlab/jupyter-collaboration/issues/252
-    await jp_serverapp.web_app.settings["jupyter_collaboration"].stop_extension()
+    await cleanup(jp_serverapp)
 
 
 async def test_room_sequential_opening(
@@ -88,6 +94,4 @@ async def test_room_sequential_opening(
     dt = await connect(file_format, file_type, file_path)
     assert dt < 1
 
-    # workaround for a shutdown issue of aiosqlite, see
-    # https://github.com/jupyterlab/jupyter-collaboration/issues/252
-    await jp_serverapp.web_app.settings["jupyter_collaboration"].stop_extension()
+    await cleanup(jp_serverapp)
