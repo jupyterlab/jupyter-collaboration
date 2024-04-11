985d30d6bb8c49b6c423e045a3ef47d8251b665f
David Brochart <david.brochart@gmail.com>
2024-04-11 18:30:30 +0200

    Support server-side execution (#279)
    * Support server-side execution
    
    * lint
    
    * again
    
    * Fix jest
    
    * Mention this feature in documentation
    
    * Update UI test snapshots
    
    * Use "dependency_type: minimum" for Minimum Versions check
    
    * Add 'serverSideExecution' to page config
    
    * Replace "fully recovered" with "recovered"
    
    * Add link to jupyter-server issue
    
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>

--- packages/collaboration-extension/src/index.ts
+++ packages/collaboration-extension/src/index.ts
@@ -12,7 +12,8 @@ import {
   menuBarPlugin,
   rtcGlobalAwarenessPlugin,
   rtcPanelPlugin,
-  userEditorCursors
+  userEditorCursors,
+  notebookCellExecutor
 } from './collaboration';
 import { sharedLink } from './sharedlink';
 
@@ -25,7 +26,8 @@ const plugins: JupyterFrontEndPlugin<any>[] = [
   rtcGlobalAwarenessPlugin,
   rtcPanelPlugin,
   sharedLink,
-  userEditorCursors
+  userEditorCursors,
+  notebookCellExecutor
 ];
 
 export default plugins;
