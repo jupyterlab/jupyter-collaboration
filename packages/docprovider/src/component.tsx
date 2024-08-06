17801958020ae916494ed401e1af2563cdac22a5
Meriem-BenIsmail <meriembenismail867@gmail.com>
2024-08-06 10:52:17 +0200

    add license header to new files

--- packages/docprovider/src/component.tsx
+++ packages/docprovider/src/component.tsx
@@ -1,3 +1,8 @@
+/* -----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+
 import React, { useState, useRef } from 'react';
 import '../style/slider.css';
 import { WebSocketProvider } from './yprovider';
