0754bc7022096e70d6a15df1eae106607ff47971
David Brochart <david.brochart@gmail.com>
2024-09-09 14:39:30 +0200

    Remove unused chat feature (#346)
    * Remove unused chat feature
    
    * Review

--- packages/docprovider/src/awareness.ts
+++ packages/docprovider/src/awareness.ts
@@ -6,26 +6,14 @@
 import { User } from '@jupyterlab/services';
 
 import { IDisposable } from '@lumino/disposable';
-import { IStream, Stream } from '@lumino/signaling';
 
 import { IAwareness } from '@jupyter/ydoc';
 
-import * as decoding from 'lib0/decoding';
-import * as encoding from 'lib0/encoding';
 import { WebsocketProvider } from 'y-websocket';
-
-import { MessageType } from './utils';
-import { IAwarenessProvider } from './tokens';
 
 export interface IContent {
   type: string;
   body: string;
-}
-
-export interface IChatMessage {
-  sender: string;
-  timestamp: number;
-  content: IContent;
 }
 
 /**
@@ -36,7 +24,7 @@ export interface IChatMessage {
  */
 export class WebSocketAwarenessProvider
   extends WebsocketProvider
-  implements IAwarenessProvider, IDisposable
+  implements IDisposable
 {
   /**
    * Construct a new WebSocketAwarenessProvider
@@ -55,31 +43,10 @@ export class WebSocketAwarenessProvider
       .then(() => this._onUserChanged(this._user))
       .catch(e => console.error(e));
     this._user.userChanged.connect(this._onUserChanged, this);
-
-    this._messageStream = new Stream(this);
-
-    this.messageHandlers[MessageType.CHAT] = (
-      encoder,
-      decoder,
-      provider,
-      emitSynced,
-      messageType
-    ) => {
-      const content = decoding.readVarString(decoder);
-      const data = JSON.parse(content) as IChatMessage;
-      this._messageStream.emit(data);
-    };
   }
 
   get isDisposed(): boolean {
     return this._isDisposed;
-  }
-
-  /**
-   * A signal to subscribe for incoming messages.
-   */
-  get messageStream(): IStream<this, IChatMessage> {
-    return this._messageStream;
   }
 
   dispose(): void {
@@ -92,22 +59,6 @@ export class WebSocketAwarenessProvider
     this.destroy();
   }
 
-  /**
-   * Send a message to every collaborator.
-   *
-   * @param msg message
-   */
-  sendMessage(msg: string): void {
-    const data: IContent = {
-      type: 'text',
-      body: msg
-    };
-    const encoder = encoding.createEncoder();
-    encoding.writeVarUint(encoder, MessageType.CHAT);
-    encoding.writeVarString(encoder, JSON.stringify(data));
-    this.ws!.send(encoding.toUint8Array(encoder));
-  }
-
   private _onUserChanged(user: User.IManager): void {
     this._awareness.setLocalStateField('user', user.identity);
   }
@@ -115,8 +66,6 @@ export class WebSocketAwarenessProvider
   private _isDisposed = false;
   private _user: User.IManager;
   private _awareness: IAwareness;
-
-  private _messageStream: Stream<this, IChatMessage>;
 }
 
 /**
