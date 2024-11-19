1f4ad46c1aa5dbcfb896eaadfc77f46338f65ae1
Meriem-BenIsmail <meriembenismail867@gmail.com>
2024-11-19 15:27:28 +0100

    added test that fails without PR but passes with it

--- ui-tests/tests/timeline-slider.spec.ts
+++ ui-tests/tests/timeline-slider.spec.ts
@@ -4,39 +4,36 @@
  */
 
 import { expect, test } from '@jupyterlab/galata';
+import { Page } from '@playwright/test';
 
 test.describe('Timeline Slider', () => {
-  test('should not show errors when opening file from path', async ({ page,  tmpPath }) => {
+
+  async function capturePageErrors(page: Page) {
+    const pageErrors: string[] = [];
+    page.on('pageerror', (error) => pageErrors.push(error.message));
+    return pageErrors;
+  }
+
+  async function openNotebook(page: Page, notebookPath: string) {
+    await page.click('text=File');
+    await page.click('.lm-Menu-itemLabel:text("Open from Path…")');
+    await page.fill(
+      'input[placeholder="/path/relative/to/jlab/root"]',
+      notebookPath
+    );
+    await page.click('.jp-Dialog-buttonLabel:text("Open")');
+    await page.waitForSelector('.jp-Notebook', { state: 'visible' });
+  }
+
+  test('should fail if there are console errors', async ({ page, tmpPath }) => {
+    const pageErrors = await capturePageErrors(page);
+
     await page.notebook.createNew();
     await page.notebook.save();
     await page.notebook.close();
 
-    // Step 1: Click on the "File" menu
-    await page.click('text=File');
-    await page.waitForSelector('.lm-Menu-itemLabel:text("Open from Path…")', { state: 'visible' });
+    await openNotebook(page, `${tmpPath}/Untitled.ipynb`);
 
-    // Step 2: Select "Open from Path..."
-    await page.click('.lm-Menu-itemLabel:text("Open from Path…")');
-
-    // Step 3: Enter the path for the notebook
-    await page.waitForSelector('input[placeholder="/path/relative/to/jlab/root"]'); 
-    await page.fill('input[placeholder="/path/relative/to/jlab/root"]', `${tmpPath}/Untitled.ipynb`);
-    await page.click('.jp-Dialog-buttonLabel:text("Open")'); 
-
-    await page.waitForSelector('.jp-Notebook', { state: 'visible' }); 
-
-
-    // Step 6: Assert that no error is thrown
-    const consoleMessages: any[] = [];
-    page.on('console', (msg) => {
-      if (msg.type() === 'error') {
-        consoleMessages.push(msg.text());
-      }
-    });
-    await page.waitForTimeout(1000);
-
-    // Ensure there are no errors in the console
-    expect(consoleMessages).toHaveLength(0);
-
+    expect(pageErrors).toHaveLength(0);
   });
 });
