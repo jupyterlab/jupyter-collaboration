7b44441f1fe4c6af9751119952129b66a2b49fc0
Meriem-BenIsmail <meriembenismail867@gmail.com>
2024-11-19 15:35:13 +0100

    added ui test that passes with PR but fails without it.

--- ui-tests/tests/timeline-slider.spec.ts
+++ ui-tests/tests/timeline-slider.spec.ts
@@ -6,24 +6,24 @@
 import { expect, test } from '@jupyterlab/galata';
 import { Page } from '@playwright/test';
 
-test.describe('Timeline Slider', () => {
+async function capturePageErrors(page: Page) {
+  const pageErrors: string[] = [];
+  page.on('pageerror', (error) => pageErrors.push(error.message));
+  return pageErrors;
+}
 
-  async function capturePageErrors(page: Page) {
-    const pageErrors: string[] = [];
-    page.on('pageerror', (error) => pageErrors.push(error.message));
-    return pageErrors;
-  }
+async function openNotebook(page: Page, notebookPath: string) {
+  await page.click('text=File');
+  await page.click('.lm-Menu-itemLabel:text("Open from Path…")');
+  await page.fill(
+    'input[placeholder="/path/relative/to/jlab/root"]',
+    notebookPath
+  );
+  await page.click('.jp-Dialog-buttonLabel:text("Open")');
+  await page.waitForSelector('.jp-Notebook', { state: 'visible' });
+}
 
-  async function openNotebook(page: Page, notebookPath: string) {
-    await page.click('text=File');
-    await page.click('.lm-Menu-itemLabel:text("Open from Path…")');
-    await page.fill(
-      'input[placeholder="/path/relative/to/jlab/root"]',
-      notebookPath
-    );
-    await page.click('.jp-Dialog-buttonLabel:text("Open")');
-    await page.waitForSelector('.jp-Notebook', { state: 'visible' });
-  }
+test.describe('Open from Path', () => {
 
   test('should fail if there are console errors', async ({ page, tmpPath }) => {
     const pageErrors = await capturePageErrors(page);
