647317e8f658c82b6f6c3a592f211a93538a3614
nzinfo <li.monan@gmail.com>
2025-01-02 00:54:00 +0800

    fix

--- packages/collaboration/src/collaboratorspanel.tsx
+++ packages/collaboration/src/collaboratorspanel.tsx
@@ -98,14 +98,19 @@ export class CollaboratorsPanel extends Panel {
   private _onAwarenessChanged = () => {
     const state = this._awareness.getStates() as any;
     const collaborators: ICollaboratorAwareness[] = [];
-
+    const collaborators_keys: Set<string> = new Set();
     state.forEach((value: Partial<ICollaboratorAwareness>, key: any) => {
       if (
         this._currentUser.isReady &&
         value.user &&
         value.user.username !== this._currentUser.identity!.username
       ) {
-        collaborators.push(value as ICollaboratorAwareness);
+        const uniqueKey = `${value.user.username}-${value.current || 'no-current'}`;
+
+        if (!collaborators_keys.has(uniqueKey)) { 
+          collaborators.push(value as ICollaboratorAwareness);
+          collaborators_keys.add(uniqueKey);
+        }
       }
     });
     this._collaboratorsChanged.emit(collaborators);
@@ -132,9 +137,11 @@ export function CollaboratorsBody(props: {
 
   return (
     <div className={COLLABORATORS_LIST_CLASS}>
-      {collaborators.map((collaborator, i) => {
+      {collaborators.map((collaborator) => {
+        const uniqueKey = `${collaborator.user.username}-${collaborator.current || 'no-current'}`;
         return (
           <Collaborator
+            key={uniqueKey}
             collaborator={collaborator}
             fileopener={props.fileopener}
             docRegistry={props.docRegistry}
