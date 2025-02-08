c437258e6b964372637157d7a68964952f2bd65b
Hyrla <gauthier.heiss@gmail.com>
2025-02-08 14:28:01 +0100

    Adding a UI to create and revoke shares to users and groups

--- packages/collaboration/src/sharedlink.ts
+++ packages/collaboration/src/sharedlink.ts
@@ -45,22 +45,412 @@ export async function showSharedLinkDialog({
     )
   );
 
-  return showDialog({
-    title: trans.__('Share Jupyter Server Link'),
-    body: new SharedLinkBody(
-      url.toString(),
-      token,
-      PageConfig.getOption('hubUser') !== '',
-      trans
-    ),
-    buttons: [
-      Dialog.cancelButton(),
-      Dialog.okButton({
-        label: trans.__('Copy Link'),
-        caption: trans.__('Copy the link to the Jupyter Server')
-      })
-    ]
-  });
+  let canCreateShare = false;
+  let canListUsers = false;
+  let canListGroups = false;
+  let hubApiUrl = '';
+
+  // If hubPrefix or hubHost or hubUser is set, we are behind a JupyterHub
+  if (
+    PageConfig.getOption('hubPrefix') ||
+    PageConfig.getOption('hubHost') ||
+    PageConfig.getOption('hubUser')
+  ) {
+    // Prepare the Hub API URL
+    const protocol = window.location.protocol;
+    const hostname =
+      PageConfig.getOption('hubHost') || window.location.hostname;
+    const port = window.location.port;
+    const prefix = PageConfig.getOption('hubPrefix');
+    hubApiUrl = `${protocol}//${hostname}:${port}${prefix}api`;
+    // Check Hub version for share compatibility (>= 5.0)
+    const response = await fetch(hubApiUrl, {
+      // A GET request on base API url returns the Hub version
+      headers: {
+        Authorization: `token ${token}`
+      }
+    });
+    const data = await response.json();
+    const hubVersion = data.version;
+    const [major] = hubVersion.split('.').map(Number);
+    if (major >= 5) {
+      // The Hub version is compatible with the share feature, but we need to check if the user has rights to create a share
+      const userResponse = await fetch(`${hubApiUrl}/user`, {
+        headers: {
+          Authorization: `token ${token}`
+        }
+      });
+      const userData = await userResponse.json();
+      // The permissions needed are "read:users:name" (to be able to get a user by his/her name) and "shares!user=self" (to be able to manage shares)
+      if (
+        userData.scopes.includes('read:users:name') &&
+        userData.scopes.some((scope: string) => scope.startsWith('shares'))
+      ) {
+        // TODO: check scopes in a cleaner way?
+        // If the user has the required permissions, we can create a share
+        canCreateShare = true;
+        // Check if the user has the correct scope to list all users (not mandatory, but makes the UI easier to use)
+        if (
+          userData.scopes.includes('list:users') ||
+          userData.scopes.includes('read:users') ||
+          userData.scopes.includes('admin:users')
+        ) {
+          canListUsers = true;
+        }
+        // Check if the user has the correct scope to list all groups (not mandatory, but makes the UI easier to use)
+        if (
+          userData.scopes.includes('list:groups') ||
+          userData.scopes.includes('read:groups') ||
+          userData.scopes.includes('admin:groups')
+        ) {
+          canListGroups = true;
+        }
+      }
+    }
+  }
+
+  // If we can create a share, open the proper UI
+  if (canCreateShare) {
+    const serverName = PageConfig.getOption('baseUrl').split(
+      PageConfig.getOption('hubUser')
+    )[1];
+    let readableServerName = serverName.replace(/\/$/, '');
+    if (readableServerName === '') {
+      readableServerName = 'default';
+    }
+    return showDialog({
+      title: trans.__('Share Jupyter Server ') + readableServerName,
+      body: new ManageSharesBody(
+        url.toString(),
+        serverName,
+        canListUsers,
+        canListGroups,
+        PageConfig.getOption('hubUser'),
+        hubApiUrl,
+        token,
+        trans
+      ),
+      buttons: [
+        Dialog.cancelButton({
+          label: trans.__('Close'),
+          caption: trans.__('Close the dialog')
+        }),
+        Dialog.okButton({
+          label: trans.__('Copy Link'),
+          caption: trans.__('Copy the link to the Jupyter Server')
+        })
+      ]
+    });
+    // If we can't create a real share, we show the legacy dialog that just copies the URL with the server owner's token
+  } else {
+    return showDialog({
+      title: trans.__('Share Jupyter Server Link'),
+      body: new SharedLinkBody(
+        url.toString(),
+        token,
+        PageConfig.getOption('hubUser') !== '',
+        trans
+      ),
+      buttons: [
+        Dialog.cancelButton(),
+        Dialog.okButton({
+          label: trans.__('Copy Link'),
+          caption: trans.__('Copy the link to the Jupyter Server')
+        })
+      ]
+    });
+  }
+}
+
+class ManageSharesBody extends Widget implements Dialog.IBodyWidget {
+  private _users: any[] = [];
+  private _shares: any[] = [];
+  private _searchInput: HTMLInputElement | null = null;
+  private _searchResults: HTMLDivElement | null = null;
+  private _sharesContainer: HTMLDivElement | null = null;
+
+  constructor(
+    private _url: string,
+    private _serverName: string,
+    private _canListUsers: boolean,
+    private _canListGroups: boolean,
+    private _hubUser: string,
+    private _hubApiUrl: string,
+    private _token: string,
+    private _trans: TranslationBundle
+  ) {
+    super();
+    this.populateBody(this.node);
+    this.addClass('jp-shared-link-body');
+    this.loadShares().then(() => {
+      this.updateSharesList().then(() => {
+        this.loadUsers();
+      });
+    });
+  }
+
+  /**
+   * Returns the input value.
+   */
+  getValue(): string {
+    return this._url;
+  }
+
+  protected onAfterAttach(msg: Message): void {
+    super.onAfterAttach(msg);
+  }
+
+  protected onBeforeDetach(msg: Message): void {
+    super.onBeforeDetach(msg);
+  }
+
+  private async loadUsers(): Promise<void> {
+    // If possible, download the users list for the UI
+    if (this._canListUsers) {
+      const usersResponse = await fetch(`${this._hubApiUrl}/users`, {
+        headers: {
+          Authorization: `token ${this._token}`
+        }
+      });
+      const usersData = await usersResponse.json();
+      const sharedUserNames = new Set(
+        this._shares
+          .filter(share => share.type === 'user')
+          .map(share => share.name)
+      );
+      // We remove from the list the current user and the users that already have a share
+      this._users = usersData
+        .filter(
+          (user: any) =>
+            user.name !== this._hubUser && !sharedUserNames.has(user.name)
+        )
+        .map((user: any) => ({ ...user, type: 'user' }));
+    }
+    // If possible, download the groups list for the UI and add them to the users list
+    if (this._canListGroups) {
+      const groupsResponse = await fetch(`${this._hubApiUrl}/groups`, {
+        headers: {
+          Authorization: `token ${this._token}`
+        }
+      });
+      const groupsData = await groupsResponse.json();
+      const sharedGroupNames = new Set(
+        this._shares
+          .filter(share => share.type === 'group')
+          .map(share => share.name)
+      );
+      this._users = this._users.concat(
+        groupsData
+          .filter((group: any) => !sharedGroupNames.has(group.name))
+          .map((group: any) => ({ name: group.name, type: 'group' }))
+      );
+    }
+
+    // Sort users and groups by name in alphabetical order
+    this._users.sort((a, b) => a.name.localeCompare(b.name));
+
+    this.updateSearchResults();
+  }
+
+  private async loadShares(): Promise<void> {
+    const sharesResponse = await fetch(
+      `${this._hubApiUrl}/shares/${this._hubUser}/${this._serverName}`,
+      {
+        headers: {
+          Authorization: `token ${this._token}`
+        }
+      }
+    );
+    const sharesData = await sharesResponse.json();
+    this._shares = sharesData.items.map((item: any) => ({
+      name: item.user?.name || item.group?.name,
+      createdAt: item.created_at,
+      type: item.user ? 'user' : 'group'
+    }));
+  }
+
+  private async createShare(sharewith: any, type: any): Promise<void> {
+    await fetch(
+      `${this._hubApiUrl}/shares/${this._hubUser}/${this._serverName}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `token ${this._token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          [type]: sharewith.name
+        })
+      }
+    );
+  }
+
+  private async deleteShare(sharewith: any, type: any): Promise<void> {
+    await fetch(
+      `${this._hubApiUrl}/shares/${this._hubUser}/${this._serverName}`,
+      {
+        method: 'PATCH',
+        headers: {
+          Authorization: `token ${this._token}`
+        },
+        body: JSON.stringify({
+          [type]: sharewith.name
+        })
+      }
+    );
+  }
+
+  private populateBody(dialogBody: HTMLElement): void {
+    // Add search input
+    const searchContainer = document.createElement('div');
+    searchContainer.classList.add('search-container');
+    this._searchInput = document.createElement('input');
+    this._searchInput.type = 'text';
+    this._searchInput.classList.add('search-input');
+    this._searchInput.placeholder = this._trans.__(
+      'Type to search for a user or a group to share your server with...'
+    );
+    this._searchInput.addEventListener('input', () => {
+      this.updateSearchResults();
+    });
+    searchContainer.appendChild(this._searchInput);
+    dialogBody.appendChild(searchContainer);
+
+    // Add search results container
+    this._searchResults = document.createElement('div');
+    this._searchResults.classList.add('search-results');
+    dialogBody.appendChild(this._searchResults);
+
+    // Add selected users container
+    this._sharesContainer = document.createElement('div');
+    this._sharesContainer.classList.add('selected-users');
+    dialogBody.appendChild(this._sharesContainer);
+
+    dialogBody.insertAdjacentHTML(
+      'beforeend',
+      `<input readonly value="${this._url}" class="url-input">`
+    );
+    dialogBody.insertAdjacentHTML('beforeend', '<br>');
+    dialogBody.insertAdjacentText(
+      'beforeend',
+      this._trans.__(
+        'Warning: Anyone you share this server with will have access to all your files, not just the currently open file.'
+      )
+    );
+    dialogBody.insertAdjacentHTML('beforeend', '<br>');
+    dialogBody.insertAdjacentText(
+      'beforeend',
+      this._trans.__(
+        'If your Hub administrator allows it, you can create another server dedicated to sharing a specific project.'
+      )
+    );
+  }
+
+  private updateSearchResults(): void {
+    if (!this._searchResults) {
+      return;
+    }
+    this._searchResults.innerHTML = '';
+    const query = this._searchInput?.value || '';
+
+    const filteredUsers = this._users.filter(
+      user =>
+        user.name.toLowerCase().includes(query.toLowerCase()) || query === ''
+    );
+
+    filteredUsers.forEach(user => {
+      const userElement = document.createElement('div');
+      userElement.classList.add('user-item');
+      userElement.textContent = user.name;
+      userElement.addEventListener('click', async () => {
+        await this.createShare(user, user.type);
+        await this.loadShares();
+        await this.updateSharesList();
+        // Removing the new user from the search results
+        const sharedUserNames = new Set(this._shares.map(share => share.name));
+        this._users = this._users.filter(
+          (user: any) =>
+            user.name !== this._hubUser && !sharedUserNames.has(user.name)
+        );
+        this.updateSearchResults();
+      });
+      this._searchResults?.appendChild(userElement);
+    });
+  }
+
+  private async updateSharesList(): Promise<void> {
+    if (!this._sharesContainer) {
+      return;
+    }
+    this._sharesContainer.innerHTML = '';
+
+    const table = document.createElement('table');
+    table.classList.add('shares-table');
+
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+      <th>${this._trans.__('Shared with')}</th>
+      <th>${this._trans.__('Shared Since')}</th>
+      <th>${this._trans.__('Actions')}</th>
+    `;
+    table.appendChild(headerRow);
+
+    if (this._shares.length === 0) {
+      const row = document.createElement('tr');
+      const cell = document.createElement('td');
+      cell.colSpan = 3;
+      cell.textContent = this._trans.__(
+        'Your server is not shared to anybody yet. You can search for users and groups above.'
+      );
+      row.appendChild(cell);
+      table.appendChild(row);
+    } else {
+      this._shares.forEach(share => {
+        const row = document.createElement('tr');
+        const formattedDate = new Date(share.createdAt).toLocaleString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit'
+        });
+
+        row.innerHTML = `
+        <td>${
+          share.type === 'group'
+            ? this._trans.__('Group ') + share.name
+            : share.name
+        }</td>
+        <td>${formattedDate}</td>
+        <td></td>
+      `;
+
+        const revokeButton = document.createElement('button');
+        revokeButton.textContent = this._trans.__('Revoke');
+        revokeButton.classList.add('jp-mod-styled');
+        revokeButton.addEventListener('click', async () => {
+          await this.deleteShare(share, share.type);
+          await this.loadShares();
+          await this.updateSharesList();
+          await this.loadUsers();
+          // Removing the new user from the search results
+          const sharedUserNames = new Set(
+            this._shares.map(share => share.name)
+          );
+          this._users = this._users.filter(
+            (user: any) =>
+              user.name !== this._hubUser && !sharedUserNames.has(user.name)
+          );
+          await this.updateSearchResults();
+        });
+
+        row.querySelector('td:last-child')?.appendChild(revokeButton);
+        table.appendChild(row);
+      });
+    }
+
+    this._sharesContainer.appendChild(table);
+  }
 }
 
 class SharedLinkBody extends Widget implements Dialog.IBodyWidget {
