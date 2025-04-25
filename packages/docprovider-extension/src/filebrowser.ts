45b9ec2ebb5e9fe2d6811757c211c84825392946
Darshan808 <pranishpoudel10@gmail.com>
2025-04-26 00:49:40 +0545

    force-autosave-by-default

--- packages/docprovider-extension/src/filebrowser.ts
+++ packages/docprovider-extension/src/filebrowser.ts
@@ -79,6 +79,11 @@ export const rtcContentProvider: JupyterFrontEndPlugin<ICollaborativeContentProv
       const docmanagerSettings = settingRegistry
         ? await settingRegistry.load('@jupyterlab/docmanager-extension:plugin')
         : null;
+
+      // Force autosave to be true by default initially
+      if (docmanagerSettings) {
+        void docmanagerSettings.set('autosave', true);
+      }
       const rtcContentProvider = new RtcContentProvider({
         apiEndpoint: '/api/contents',
         serverSettings: defaultDrive.serverSettings,
