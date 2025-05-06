272573da2a3d9c88947dbc735b6b5e9349d77294
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-05-06 13:47:03 +0200

    Add dialog to update user info

--- packages/collaboration/src/userinfopanel.tsx
+++ packages/collaboration/src/userinfopanel.tsx
@@ -1,15 +1,16 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { ReactWidget } from '@jupyterlab/apputils';
+import { Dialog, ReactWidget, showDialog } from '@jupyterlab/apputils';
 
-import { User } from '@jupyterlab/services';
+import { ServerConnection, User } from '@jupyterlab/services';
 
 import { Panel } from '@lumino/widgets';
 
 import * as React from 'react';
 
-import { UserIconComponent } from './components';
+import { UserDetailsBody, UserIconComponent } from './components';
+import { URLExt } from '@jupyterlab/coreutils';
 
 export class UserInfoPanel extends Panel {
   private _profile: User.IManager;
@@ -23,13 +24,13 @@ export class UserInfoPanel extends Panel {
     this._body = null;
 
     if (this._profile.isReady) {
-      this._body = new UserInfoBody(this._profile.identity!);
+      this._body = new UserInfoBody({ userManager: this._profile });
       this.addWidget(this._body);
       this.update();
     } else {
       this._profile.ready
         .then(() => {
-          this._body = new UserInfoBody(this._profile.identity!);
+          this._body = new UserInfoBody({ userManager: this._profile });
           this.addWidget(this._body);
           this.update();
         })
@@ -39,29 +40,86 @@ export class UserInfoPanel extends Panel {
 }
 
 /**
+ * The properties for the UserInfoBody.
+ */
+type UserInfoBodyProps = {
+  userManager: User.IManager;
+};
+
+/**
  * A SettingsWidget for the user.
  */
-export class UserInfoBody extends ReactWidget {
-  private _user: User.IIdentity;
+export class UserInfoBody
+  extends ReactWidget
+  implements Dialog.IBodyWidget<User.IManager>
+{
+  private _userManager: User.IManager;
 
   /**
    * Constructs a new settings widget.
    */
-  constructor(user: User.IIdentity) {
+  constructor(props: UserInfoBodyProps) {
     super();
-    this._user = user;
+    this._userManager = props.userManager;
   }
 
-  get user(): User.IIdentity {
-    return this._user;
+  get user(): User.IManager {
+    return this._userManager;
   }
 
-  set user(user: User.IIdentity) {
-    this._user = user;
+  set user(user: User.IManager) {
+    this._userManager = user;
     this.update();
   }
 
+  private onClick = () => {
+    if (!this._userManager.identity) {
+      return;
+    }
+    showDialog({
+      body: new UserDetailsBody({
+        userManager: this._userManager
+      }),
+      title: 'User Details'
+    }).then(async result => {
+      if (result.button.accept) {
+        // Call the Jupyter Server API to update the user field
+        try {
+          const settings = ServerConnection.makeSettings();
+          const url = URLExt.join(settings.baseUrl, '/api/me');
+          const body = {
+            method: 'PATCH',
+            body: JSON.stringify(result.value)
+          };
+
+          let response: Response;
+          try {
+            response = await ServerConnection.makeRequest(url, body, settings);
+          } catch (error) {
+            throw new ServerConnection.NetworkError(error as Error);
+          }
+
+          if (!response.ok) {
+            throw new Error('Failed to update user data');
+          }
+
+          // Refresh user information
+          this._userManager.refreshUser();
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  };
+
   render(): JSX.Element {
-    return <UserIconComponent user={this._user} />;
+    return (
+      <div className="jp-UserInfo-Container">
+        <UserIconComponent
+          userManager={this._userManager}
+          onClick={this.onClick}
+        />
+      </div>
+    );
   }
 }
