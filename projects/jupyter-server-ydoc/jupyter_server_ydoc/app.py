dbef400d2e8f3fc4bc9695e8316e5fa040eb1404
darshan <pranishpoudel10@gmail.com>
2026-03-15 22:09:13 +0545

    test-linter

--- projects/jupyter-server-ydoc/jupyter_server_ydoc/app.py
+++ projects/jupyter-server-ydoc/jupyter_server_ydoc/app.py
@@ -2,16 +2,17 @@
 # Distributed under the terms of the Modified BSD License.
 from __future__ import annotations
 
-import asyncio
 from functools import partial
 from typing import Literal
+import asyncio
 
+from traitlets import Bool, Float, Type
 from jupyter_server.extension.application import ExtensionApp
 from jupyter_ydoc import ydocs as YDOCS
-from jupyter_ydoc.ybasedoc import YBaseDoc
 from pycrdt import Doc
+from jupyter_ydoc.ybasedoc import YBaseDoc
+from .loaders import FileLoaderMapping
 from pycrdt.store import BaseYStore
-from traitlets import Bool, Float, Type
 
 from .handlers import (
     DocForkHandler,
@@ -20,7 +21,7 @@ from .handlers import (
     UndoRedoHandler,
     YDocWebSocketHandler,
 )
-from .loaders import FileLoaderMapping
+from .websocketserver import JupyterWebsocketServer, RoomNotFound, exception_logger
 from .rooms import DocumentRoom
 from .stores import SQLiteYStore
 from .utils import (
@@ -30,7 +31,6 @@ from .utils import (
     encode_file_path,
     room_id_from_encoded_path,
 )
-from .websocketserver import JupyterWebsocketServer, RoomNotFound, exception_logger
 
 
 class YDocExtension(ExtensionApp):
