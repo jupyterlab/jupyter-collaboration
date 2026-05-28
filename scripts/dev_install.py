493aab1d952e6e6b630d7f10f0891c708e0fa7d3
David Brochart <david.brochart@gmail.com>
2026-05-28 17:58:49 +0200

    Update jupyterlab versions

--- scripts/dev_install.py
+++ scripts/dev_install.py
@@ -10,10 +10,7 @@ def execute(cmd: str, cwd: Path | None = None) -> None:
 
 
 def install_dev() -> None:
-    install_build_deps = (
-        "python -m pip install jupyterlab>=4.4.0,<5 hatchling>=1.4.0 hatch-jupyter-builder>=0.5 "
-        "editables"
-    )
+    install_build_deps = "python -m pip install jupyterlab>=4.4.0,<5"
     install_js_deps = "jlpm install"
 
     python_package_prefix = "projects"
@@ -25,7 +22,7 @@ def install_dev() -> None:
     for py_package in python_packages:
         real_package_name = py_package.replace("-", "_")
         execute(f"pip uninstall {real_package_name} -y")
-        execute(f"pip install --no-build-isolation -e {python_package_prefix}/{py_package}[test]")
+        execute(f"pip install -e {python_package_prefix}/{py_package}[test]")
 
         # List of server extensions
         if py_package in ["jupyter-server-ydoc"]:
