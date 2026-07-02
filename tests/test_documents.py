595b6e43f6654ca9532a593bdef0a4e6139def09
David Brochart <david.brochart@gmail.com>
2026-07-02 22:33:04 +0200

    again

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -64,11 +64,15 @@ async def test_dirty(
 
     websocket, room_name = await rtc_connect_doc_client(file_format, file_type, file_path)
     async with websocket as ws, Provider(jupyter_ydoc.ydoc, HttpxWebsocket(ws, room_name)):
-        await sleep(0.2)
-        for _ in range(2):
-            jupyter_ydoc.dirty = True
-            await sleep(rtc_document_save_delay * 1.5)
-            assert not jupyter_ydoc.dirty
+        # the server might not be ready to receive updates from this client yet
+        jupyter_ydoc.dirty = True
+        await sleep(rtc_document_save_delay * 1.5)
+        # so don't expect dirty to be cleared
+
+        # now the server should be ready, check that dirty is cleared
+        jupyter_ydoc.dirty = True
+        await sleep(rtc_document_save_delay * 1.5)
+        assert not jupyter_ydoc.dirty
 
 
 async def cleanup(jp_serverapp):
